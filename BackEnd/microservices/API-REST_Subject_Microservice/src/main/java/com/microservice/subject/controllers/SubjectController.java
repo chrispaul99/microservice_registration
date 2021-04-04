ac877e61a9dfc09ee0d1b9bef4e55fa07959894d
Chris <paul19992012@gmail.com>
2021-04-04 04:41:42 -0500

    cors

--- BackEnd/microservices/API-REST_Subject_Microservice/src/main/java/com/microservice/subject/controllers/SubjectController.java
+++ BackEnd/microservices/API-REST_Subject_Microservice/src/main/java/com/microservice/subject/controllers/SubjectController.java
@@ -3,13 +3,11 @@ package com.microservice.subject.controllers;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
