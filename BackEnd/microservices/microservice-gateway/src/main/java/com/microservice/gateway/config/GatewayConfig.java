0f60cc69c5414fd1f0a9fda34cd02b17702c196f
MauroVRuizM <59812652+MauroVRuizM@users.noreply.github.com>
2021-04-06 00:20:49 -0500

    Pre cambios Cors Backend

--- BackEnd/microservices/microservice-gateway/src/main/java/com/microservice/gateway/config/GatewayConfig.java
+++ BackEnd/microservices/microservice-gateway/src/main/java/com/microservice/gateway/config/GatewayConfig.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins = "*")
 @EnableHystrix
 public class GatewayConfig {
 
